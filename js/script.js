05eca45827112916e8ff82bae34ef2140b8b612b
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-03 15:21:26 +0200

    add variabili fizz e buzz

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi anche "Fizz" di fianco al numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
 
+    var fizz = 'Fizz';
+    var buzz = 'Buzz';
 for (i = 1; i <= 100; i++) {
     if ( i % 3 == 0 && i % 5 == 0) {
-        document.getElementById('numeri').innerHTML += i + ' FizzBuzz' + '<br>';
+        document.getElementById('numeri').innerHTML += i + ' ' + fizz + buzz + '<br>';
     } else if (i % 5 == 0) {
-        document.getElementById('numeri').innerHTML += i + ' Buzz' + '<br>';
+        document.getElementById('numeri').innerHTML += i + ' ' + buzz + '<br>';
     } else if (i % 3 == 0) {
-        document.getElementById('numeri').innerHTML += i + ' Fizz' + '<br>';
+        document.getElementById('numeri').innerHTML += i + ' ' + fizz + '<br>';
     } else {
         document.getElementById('numeri').innerHTML += i + '<br>';
     }
