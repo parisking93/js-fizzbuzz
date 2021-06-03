21a06fa39441a993b045b6815acd76cc1bb3662d
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-03 15:05:41 +0200

    add numeri da 1 a 10o + fizz + buzz + fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi anche "Fizz" di fianco al numero e per i multipli di 5 stampi "Buzz". Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
 
-
-
-console.log('ciao');
+for(i = 1; i <= 100; i++) {
+    if ( i % 3 == 0 && i % 5 == 0) {
+        document.getElementById('numeri').innerHTML += i + ' FizzBuzz' + '<br>';
+    } else if (i % 5 == 0) {
+        document.getElementById('numeri').innerHTML += i + ' Buzz' + '<br>';
+    } else if (i % 3 == 0) {
+        document.getElementById('numeri').innerHTML += i + ' Fizz' + '<br>';
+    }
+}
 
 
 
