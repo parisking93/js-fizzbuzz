5c6bc6011617e955b8a2e2ccff07e2f132cc6231
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-03 16:38:04 +0200

    add modulo 6

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@
 
     var fizz = 'Fizz';
     var buzz = 'Buzz';
+    var boolean = 'Boolean';
 for (i = 1; i <= 100; i++) {
-    if ( i % 3 == 0 && i % 5 == 0) {
-        document.getElementById('numeri').innerHTML += i + ' ' + fizz + buzz + '<br>';
+    if ( i % 3 == 0 && i % 5 == 0 && i % 6 == 0) {
+        document.getElementById('numeri').innerHTML += i + ' ' + fizz + buzz + boolean + '<br>';
+    } else if (i % 6 == 0 && i % 3 == 0 ) {
+        document.getElementById('numeri').innerHTML += i + ' ' + fizz + boolean + '<br>';
     } else if (i % 5 == 0) {
         document.getElementById('numeri').innerHTML += i + ' ' + buzz + '<br>';
-    } else if (i % 3 == 0) {
+    } else if (i % 3 == 0 ) {
         document.getElementById('numeri').innerHTML += i + ' ' + fizz + '<br>';
     } else {
         document.getElementById('numeri').innerHTML += i + '<br>';
